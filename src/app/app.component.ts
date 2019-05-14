9b95aab86b88d7c7d017a9672f8070e86aabcd3c
Jamie Hsu <shiyating@gmail.com>
2019-05-14 10:09:25 +0800

    [Summary] display blocks and show your turn content [Type] [Scope] [Bug] [Phenomemon]
    [Reason]
    
    [Changes]
    
    [Announcement]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Block } from './block';
+import { BLOCKS } from './mock-blocks';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+
+  blocks = BLOCKS;
+
+  turn = 'o';
+
 }
